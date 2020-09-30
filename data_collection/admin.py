1ef7924e756077118a1f8ca17a223164640eff08
longclaw <shivam.singhal212@gmail.com>
2020-10-01 03:50:45 +0530

    Add: admin support

--- data_collection/admin.py
+++ data_collection/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from data_collection.models import ProductCatalogue, DailyProductData
 # Register your models here.
+admin.site.register(ProductCatalogue)
+admin.site.register(DailyProductData)
